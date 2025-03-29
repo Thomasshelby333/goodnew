622fd8ef9a3b5f3869d167a42990363c6b6c45b2
Thomasshelby333 <116047364+Thomasshelby333@users.noreply.github.com>
2025-03-29 17:03:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "600")) # Time in Seconds
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002587721890"))
 
 # File Caption Information
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", (f"{script.CAPTION}")
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION",f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 
 # Enable - True or Disable - False
