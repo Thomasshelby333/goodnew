84ceca065487224247a2369be251e888ade67535
Thomasshelby333 <116047364+Thomasshelby333@users.noreply.github.com>
2025-04-13 14:52:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -24,18 +24,6 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
-async def is_subscribed(bot, query, channel):
-    btn = []
-    for id in channel:
-        chat = await bot.get_chat(int(id))
-        try:
-            await bot.get_chat_member(id, query.from_user.id)
-        except UserNotParticipant:
-            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
-        except Exception as e:
-            pass
-    return btn
-
 def get_size(size):
     """Get size in readable format"""
 
